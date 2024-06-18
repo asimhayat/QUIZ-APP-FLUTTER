21245d67b5f09fa2823d7bd9ba7b6531a7c37113
AsimHayat <asimhayat125@gmail.com>
2024-06-19 01:18:59 +0500

    Quiz app Using FLUTTER

---
+++ quizapp/lib/models/questions.dart
@@ -0,0 +1,106 @@
+import 'package:quizapp/models/question.dart';
+
+const List<Question> questions = [
+  Question(
+    question: '1. What does the keyword "var" in JavaScript declare?',
+    correctAnswerIndex: 0,
+    options: [
+      'a) A variable',
+      'b) A constant',
+      'c) A function',
+      'd) A class',
+    ],
+  ),
+  Question(
+    question:
+        '2. Which programming language is known as the backbone of web development?',
+    correctAnswerIndex: 2,
+    options: [
+      'a) Python',
+      'b) C++',
+      'c) JavaScript',
+      'd) Java',
+    ],
+  ),
+  Question(
+    question: '3. What is the main purpose of the "git" command?',
+    correctAnswerIndex: 1,
+    options: [
+      'a) Compile code',
+      'b) Version control',
+      'c) Debug code',
+      'd) Execute scripts',
+    ],
+  ),
+  Question(
+    question: '4. In Python, what does the "def" keyword define?',
+    correctAnswerIndex: 2,
+    options: [
+      'a) A loop',
+      'b) A class',
+      'c) A function',
+      'd) A module',
+    ],
+  ),
+  Question(
+    question: '5. What does CSS stand for?',
+    correctAnswerIndex: 3,
+    options: [
+      'a) Computer Style Sheets',
+      'b) Creative Style Sheets',
+      'c) Cascading Simple Sheets',
+      'd) Cascading Style Sheets',
+    ],
+  ),
+  Question(
+    question:
+        '6. Which language is primarily used for Android app development?',
+    correctAnswerIndex: 1,
+    options: [
+      'a) Swift',
+      'b) Java',
+      'c) Ruby',
+      'd) C#',
+    ],
+  ),
+  Question(
+    question: '7. What is the main use of SQL?',
+    correctAnswerIndex: 0,
+    options: [
+      'a) Managing databases',
+      'b) Designing websites',
+      'c) Writing applications',
+      'd) Creating graphics',
+    ],
+  ),
+  Question(
+    question: '8. Which of the following is a Python web framework?',
+    correctAnswerIndex: 2,
+    options: [
+      'a) Angular',
+      'b) React',
+      'c) Django',
+      'd) Laravel',
+    ],
+  ),
+  Question(
+    question: '9. What is the main purpose of HTML?',
+    correctAnswerIndex: 1,
+    options: [
+      'a) Styling web pages',
+      'b) Structuring web content',
+      'c) Programming logic',
+      'd) Managing databases',
+    ],
+  ),
+  Question(
+    question: '10. In which language is TensorFlow written?',
+    correctAnswerIndex: 0,
+    options: [
+      'a) Python',
+      'b) Java',
+      'c) C++',
+      'd) JavaScript',
+    ],
+  ),
+];
